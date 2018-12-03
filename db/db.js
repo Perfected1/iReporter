addacd66e79a0068b3abb6443543da50b2d05b84
Perfected1 <chikennamadim65@gmail.com>
2018-12-04 00:01:49 +0100

    add dummy database for Reports

---
+++ db/db.js
@@ -0,0 +1,51 @@
+const reports = [{
+        id: 1,
+        createdOn: 23 - 11 - 2108, //date
+        createdBy: "Chike Nnamadim",
+        title: "Internet Fruad",
+        type: "Red-flag",
+        location: "lat, long",
+        status: "Approved",
+        images: "image, image",
+        video: "video, video",
+        comment: "A red flag record. that talk about Internet fraud and how it affects our society"
+    },
+    {
+        id: 2,
+        createdOn: 23 / 11 / 2108, //date
+        createdBy: "Chike Nnamadim",
+        title: "Internet Fruad",
+        type: "Red-flag",
+        location: "lat, long",
+        status: "Approved",
+        images: "image, image",
+        video: "video, video",
+        comment: "A red flag record. that talk about Internet fraud and how it affects our society"
+    },
+    {
+        id: 3,
+        createdOn: 23 / 11 / 2108, //date
+        createdBy: "Chike Nnamadim",
+        title: "Poor Power Supply",
+        type: "Red-flag",
+        location: "lat, long",
+        status: "Approved",
+        images: "image, image",
+        video: "video, video",
+        comment: "Poor Power supply is becoming a major issue, mostly because citizen now pay more and get less!"
+    },
+    {
+        id: 4,
+        createdOn: 23 / 11 / 2108, //date
+        createdBy: "Chike Nnamadim",
+        title: "Pot holes",
+        type: "Red-flag",
+        location: "lat, long",
+        status: "Approved",
+        images: "image, image",
+        video: "video, video",
+        comment: "A red flag record. that talk about Internet fraud and how it affects our society"
+    }
+]
+
+export default reports;
