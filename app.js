9d05456578924d97778eff8b4f638c67a48e182c
Perfected1 <chikennamadim65@gmail.com>
2018-12-04 07:35:42 +0100

    add endpoint for getting individual/specific report

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 import express from'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
 
 //set up the express app
 const app = express();
 
+//Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//end point for getting all reports
 app.get('/api/v1/reports', (req, res) => {
     res.status(200).send({
         success: 'true',
         message: 'reports retrieved successfully!',
         reports: db
     })
+});
+
+//endpoint for getting a specific report
+app.get('/api/v1/reports/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    db.map((reports) => {
+        if (reports.id === id) {
+            return res.status(200).send({
+                success: 'true',
+                message: 'report retrieved successfully!',
+                reports,
+            });
+        }
+    });
+    return res.status(404).send({
+        success: 'false',
+        message: 'report does not exist!',
+    });
 });
 
 const PORT = 5000;
