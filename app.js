84c09434c2e6cbda4c767943e2b8490399af6a0f
Chike Nnamadim <chikennamadim65@gmail.com>
2018-12-07 09:43:02 +0100

    add route homepage

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ import router from './routes/index.js';
 //set up the express app
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json('Welcome to iReporter News Platform');
+  });
+
 //Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 app.use(router);
+
+app.all('*', (req, res) => {
+    res.json('Route not available at the moment');
+  });
 
 const PORT = 5000;
 
